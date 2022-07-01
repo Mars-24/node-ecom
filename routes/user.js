c2741314d697e35b35a06929e9731ebf9be52f33
Mars-24 <condormatt6@gmail.com>
2022-07-01 00:16:43 +0000

    adding product api

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
         const updateUser = await User.findByIdAndUpdate(req.params.id, {
             $set: req.body
         }, { new: true });
-        res.status(200).json("User has been deleted");
+        res.status(200).json(updateUser);
     } catch (err) {
         res.status(500).json(err)
     }
@@ -20,7 +20,7 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
 router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json
+        res.status(200).json("User has been deleted")
     } catch (error) {
         res.status(500).json(error);
     }
@@ -51,14 +51,29 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
 
 //GET USER STATS
 
-router.get('/stats', verifyTokenAndAdmin, (req, res) => {
-    const date = Date();
-    const lastYear = new Date(date.getFullYear(date.getFullYear(-1)));
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
     try {
-        const data = await User.aggregate([{ $match: { createdAt: { $gte: lastYear } } },
-        {}
-        ])
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+                $project: {
+                    month: {
+                        $month: "$createdAt"
+                    },
+                },
+
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 },
+                }
+            }
+        ]);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json(error);
     }
